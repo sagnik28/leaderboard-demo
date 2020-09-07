26cc44d51321e8b1dd18f7d831d8e02aa6e1635a
sagnik28 <sagnikaditya281@gmail.com>
2020-09-07 15:01:05 +0200

    Changed Access Patterns

--- cruncher.c
+++ cruncher.c
@@ -87,69 +87,75 @@ void query(unsigned short qid, unsigned short artist, unsigned short areltd[], u
 	unsigned long knows_offset, knows_offset2;
 
 	Person *person, *knows;
-	unsigned char score;
+	char score, *score_cache = (char*) malloc(person_length/sizeof(Person));
 
-	unsigned int result_length = 0, result_idx, result_set_size = 1000;
-	Result* results = malloc(result_set_size * sizeof (Result));
-	printf("Running query %d\n", qid);
+		unsigned int result_length = 0, result_idx, result_set_size = 1000;
+		Result* results = malloc(result_set_size * sizeof (Result));
+		printf("Running query %d\n", qid);
 
+	// pass 1: just sequentially access person.bin and interest.bin
 	for (person_offset = 0; person_offset < person_length/sizeof(Person); person_offset++) {
 		person = &person_map[person_offset];
-
-		if (person_offset > 0 && person_offset % REPORTING_N == 0) {
-			printf("%.2f%%\n", 100 * (person_offset * 1.0/(person_length/sizeof(Person))));
-		}
-		// person must like some of these other guys
-		score = get_score(person, areltd);
-		if (score < 1) continue;
-
+			if (person_offset > 0 && person_offset % REPORTING_N == 0) {
+				printf("%.2f%%\n", 100 * (person_offset * 1.0/(person_length/sizeof(Person))));
+			}
+        score_cache[person_offset] = -1;
 		// person must not like artist yet
 		if (likes_artist(person, artist)) continue;
-
+		score_cache[person_offset] = get_score(person, areltd);
+	}
+  
+    // pass 2: just sequentially access person.bin and knows.bin
+    	for (person_offset = 0; person_offset < person_length/sizeof(Person); person_offset++) {
+			person = &person_map[person_offset];
+			//filter by birthday
+			if (person->birthday < bdstart || person->birthday > bdend) continue;
+		// but person must like some of these other guys
+		score = score_cache[person_offset];
+		if (score < 1) continue;
 		// check if friend lives in same city and likes artist 
 		for (knows_offset = person->knows_first; 
 			knows_offset < person->knows_first + person->knows_n; 
 			knows_offset++) {
-
-			knows = &person_map[knows_map[knows_offset]];
-			if (person->location != knows->location) continue; 
-
+				unsigned int friend_offset=knows_map[knows_offset];
+            knows =&person_map[friend_offset];
+			// check if same location
+			if (person->location !=knows->location) continue; 
 			// friend must already like the artist
-			if (!likes_artist(knows, artist)) continue;
-
-			// friendship must be mutual
-			for (knows_offset2 = knows->knows_first;
-				knows_offset2 < knows->knows_first + knows->knows_n;
-				knows_offset2++) {
-			
-				if (knows_map[knows_offset2] == person_offset) {
-					// filter by birthday
-					if (person->birthday < bdstart || person->birthday > bdend) continue; 
-
-					// realloc result array if we run out of space
-					if (result_length >= result_set_size) {
-						result_set_size *= 2;
-						results = realloc(results, result_set_size * sizeof (Result));
-					}
-					results[result_length].person_id = person->person_id;
-					results[result_length].knows_id = knows->person_id;
-					results[result_length].score = score;
-					result_length++;
-					break;
-				}
+			if (score_cache[friend_offset] != -1) continue;
+			// realloc result array if we run out of space
+			if (result_length >= result_set_size) {
+				result_set_size *= 2;
+				results = realloc(results, result_set_size * sizeof (Result));
 			}
+			results[result_length].person_id = person_offset;
+			results[result_length].knows_id = friend_offset;
+			results[result_length].score = score;
+			result_length++;
 		}
 	}
-
 	// sort result
-	qsort(results, result_length, sizeof(Result), &result_comparator);
-
-	// output
+		qsort(results, result_length, sizeof(Result), &result_comparator);
+	// pass 3: Do random access to knows.bin (but isolated)
 	for (result_idx = 0; result_idx < result_length; result_idx++) {
-		fprintf(outfile, "%d|%d|%lu|%lu\n", qid, results[result_idx].score, 
-			results[result_idx].person_id, results[result_idx].knows_id);
+		person_offset = results[result_idx].person_id;
+			knows_offset = results[result_idx].knows_id;
+		person = &person_map[person_offset];
+			knows = &person_map[knows_offset];
+	
+		for (knows_offset2 = knows->knows_first;
+			knows_offset2 < knows->knows_first + knows->knows_n;
+			knows_offset2++) {
+		
+				if (knows_map[knows_offset2] == person_offset) {
+					fprintf(outfile, "%d|%d|%lu|%lu\n", qid, results[result_idx].score, 
+					person->person_id, knows->person_id);
+					break;
+				}
+		}
 	}
 }
+
 
 void query_line_handler(unsigned char nfields, char** tokens) {
 	unsigned short q_id, q_artist, q_bdaystart, q_bdayend;
